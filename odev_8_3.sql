9266a3f71b673befb78cafe6b5b8a89c0362508a
Bora Esen <stat.bora@gmail.com>
2022-07-21 14:07:49 +0300

    Update odev_8_3.sql

--- odev_8_3.sql
+++ odev_8_3.sql
@@ -2,21 +2,21 @@ UPDATE employee
 SET birthday='2003-04-01',
     email='mirabella@gmail.com'
 WHERE birthday='2003-04-11';
------------------------------
+
 UPDATE employee
 SET name='invalid'
 WHERE birthday> '1999-12-31';
------------------------------
+
 UPDATE employee
 SET name='Okan',
     birthday='1973-10-19',
     email='okan1973@gmail.com'
 WHERE id=23;
------------------------------
+
 UPDATE employee
 SET name='QQQ'
 WHERE name LIKE '%t';
------------------------------
+
 UPDATE employee
 SET name='Paul',
     birthday='1997-02-05'
